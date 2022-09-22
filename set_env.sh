e1c624b57693d44f340e74f6fdee88d48c9f23e2
sixtyfive0 <sixtyfive0@gmail.com>
2022-09-22 09:42:44 +0100

    Initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=popessamuel
+export POSTGRES_PASSWORD=popessamuel
+export POSTGRES_HOST=popessamuel.c8p8h4ewck4u.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::project3-682247598780-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
